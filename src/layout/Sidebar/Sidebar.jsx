2ab0cbd777bd2c8ffe3b8eedab16f02f316f77f5
elahe-toraj <torajelahe@gmail.com>
2025-05-22 17:53:49 +0330

    add-Sidebar

---
+++ src/layout/Sidebar/Sidebar.jsx
@@ -0,0 +1,32 @@
+import "./Sidebar.css"; 
+import { personsImgs } from '../../utils/images';
+import { navigationLinks } from '../../data/data';
+const Sidebar = () => {
+    return (
+    <div className={ `sidebar` }>
+      <div className="user-info">
+          <div className="info-img img-fit-cover">
+              <img src={ personsImgs.person_two } alt="profile image" />
+          </div>
+          <span className="info-name">Elahe Toraj</span>
+      </div>
+
+      <nav className="navigation">
+          <ul className="nav-list">
+            {
+              navigationLinks.map((navigationLink) => (
+                <li className="nav-item" key = { navigationLink.id }>
+                  <a href="#" className={ `nav-link ${ navigationLink.id }` }>
+                      <img src={ navigationLink.image } className="nav-link-icon" alt = { navigationLink.title } />
+                      <span className="nav-link-text">{ navigationLink.title }</span>
+                  </a>
+                </li>
+              ))
+            }
+          </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar
